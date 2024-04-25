d3ff2cd434e6f21783ce05c290689108ce6127da
Miles Wildmore <mileswildmore18@gmail.com>
2024-04-25 15:01:38 -0400

    Added an inquirer 8.2.4 and init. As well as adding a headstart to the index.js along with the prompt.

---
+++ index.js
@@ -0,0 +1,21 @@
+const inquirer = require('inquirer');
+const fs = require ('fs');
+const { Circle, Triangle, Square } = 
+inquirer
+.prompt([
+    {type: 'input',
+     name: 'fillcolor',
+     message: 'What color would you like to use to fill your circle?',
+    }
+])
+
+.then
+let
+switch (key) {
+    case value:
+        
+        break;
+
+    default:
+        break;
+}
