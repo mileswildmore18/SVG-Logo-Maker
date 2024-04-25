fd745983788c0244a3e4d6ec9e608d98ca74747e
Miles Wildmore <mileswildmore18@gmail.com>
2024-04-25 18:50:44 -0400

    Added prompts for the user to add in for the svg file in the index.js file. Also added functions to write the file of the suer inputs into the SVG file as will as initializing the app. Also added the pathway to bring the shape properties connected to the shapes.js file.

--- index.js
+++ index.js
@@ -1,21 +1,60 @@
+//Provides all the npm install recommended to use this program
 const inquirer = require('inquirer');
-const fs = require ('fs');
-const { Circle, Triangle, Square } = 
+const fs = require('fs');
+//const { Circle, Triangle, Square } = ;
+const colors = require('colors');
+//Provides the prompts to fill in the answers the user provides
 inquirer
-.prompt([
-    {type: 'input',
-     name: 'fillcolor',
-     message: 'What color would you like to use to fill your circle?',
-    }
-])
+    .prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'What characters would you like to use for your text?',
+            validate: function(input) {
+                if (input.length <= 3) {
+                    return true;
+                } else {
+                    return 'Please enter up to three characters';
+                }
+            }
+        },
 
-.then
-let
-switch (key) {
-    case value:
-        
-        break;
+        {
+            type: 'input',
+            name: 'textColor',
+            message: 'What color would you like to use for your text?',
+        },
+        {
+            type: 'list',
+            name: 'fillcolor',
+            message: 'What shape would you like to use?',
+            choices: ['circle', 'triangle', 'square'],
+        },
 
-    default:
-        break;
+        {
+            type: 'input',
+            name: 'fillcolor',
+            message: 'What color would you like to use to fill your shape?',
+        }
+    ])
+
+//Creates a function to write a SVG file
+function writeToFile(filename, data) {
+fs.writeFile('logo.svg', data, (error) => {
+    if(error) throw error;
+})    
 }
+//Creates a function to initialize app
+function init() { }
+    //asks the questions
+inquirer.prompt(questions).then((answers) => {
+    //passes the answers to the next string
+    const string = shapes(answers)
+    //writes the file with that string
+    writeToFile('logo.svg', string)
+})
+
+//Function call to initialize app
+init();
+
+const shapes= require('./lib/shapes');
