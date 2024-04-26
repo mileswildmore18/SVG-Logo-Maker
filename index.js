8bb8567cac209f1833e81b246710e43baca4c8c3
Miles Wildmore <mileswildmore18@gmail.com>
2024-04-26 10:08:22 -0400

    added the return inquirer on index.js to bring in information added from the prompt on the CLI file.

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 
+const { default: inquirer } = require("inquirer");
 const CLI = require("./lib/CLI");
 
 new CLI().run();
+run(); {
+    return inquirer
+    .prompt()
+    .then()
+    .catch()
+}
+
+module.exports = CLI;
