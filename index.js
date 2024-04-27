3b08f8af48aeebd089fea545c2f8433f16295fd4
Miles Wildmore <mileswildmore18@gmail.com>
2024-04-26 23:48:21 -0400

    Added classes for the shapes with inheritance in the shape.js file. Also added the questions for the user to answer and to create a function to create a SVG file for the user based on the options added by the user.

--- index.js
+++ index.js
@@ -1,49 +1,76 @@
+//Gather the packages needed for this project to function
+const inquirer = require('inquirer')
+const fs = require('fs')
+const { Circle, Triangle, Square } = require('./lib/shapes')
+const SVG = require('./lib/svg')
 
-// const { default: inquirer } = require("inquirer");
-// const CLI = require("./lib/CLI");
+// Generating information from the questions answered 
+function start() {
 
-// new CLI().run();
-// // run(); {
-//     return inquirer
-//     .prompt()
-//     .then()
-//     .catch()
-// }
 
-return inquirer
-            .prompt([
-                {
-                    type: 'input',
-                    name: 'text',
-                    message: 'What characters would you like to use for your text?',
-                    validate: function (input) {
-                        if (input.length <= 3) {
-                            return true;
-                        } else {
-                            return 'Please enter up to three characters';
-                        }
+
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'text',
+                message: 'What characters would you like to use for your text?',
+                validate: function (input) {
+                    if (input.length <= 3) {
+                        return true;
+                    } else {
+                        return 'Please enter up to three characters';
                     }
-                },
-
-                {
-                    type: 'input',
-                    name: 'textColor',
-                    message: 'What color would you like to use for your text?',
-                },
-                {
-                    type: 'list',
-                    name: 'shape',
-                    message: 'What shape would you like to use?',
-                    choices: shapeChoices,
-                },
-
-                {
-                    type: 'input',
-                    name: 'shapecolor',
-                    message: 'What color would you like to use to fill your shape?',
                 }
+            },
 
-            ])
-            .then((response) => {
-                shapes.createSVG(response.text, response.textcolor, response.shape, response.shapecolor)
-            });
+            {
+                type: 'input',
+                name: 'textColor',
+                message: 'What color would you like to use for your text?',
+            },
+            {
+                type: 'list',
+                name: 'shape',
+                message: 'What shape would you like to use?',
+                choices: ['Circle', 'Triangle', 'Square']
+            },
+
+            {
+                type: 'input',
+                name: 'shapecolor',
+                message: 'What color would you like to use to fill your shape?',
+            }
+
+        ])
+        //Choosing the shape
+        .then((response) => {
+            let userChoice;
+            switch (response.shape) {
+                case 'Circle':
+                    userChoice = new Circle()
+                    break;
+                case 'Triangle':
+                    userChoice = new Triangle()
+                    break;
+                case 'Square':
+                    userChoice = new Square()
+                    break;
+
+
+            }
+            //Creates a copy from the method of the shape and text color
+            userChoice.setColor(response.shapecolor)
+            const myCoolLogo = new SVG()
+            myCoolLogo.setShape(userChoice)
+            myCoolLogo.setText(response.text, response.textColor)
+
+            //Creates a new SVG file
+            fs.writeFile('myCoolLogo.svg', myCoolLogo.render(), (err) => {
+                err ? console.error(err) : console.log('Your cool logo is generated!')
+            })
+        });
+}
+
+
+start();
