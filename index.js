a56e392536c65ddbf5bb62f9cd7fc713c9a937b4
Miles Wildmore <mileswildmore18@gmail.com>
2024-06-21 15:54:12 -0400

    added comments to explain what each function does

--- index.js
+++ index.js
@@ -15,8 +15,10 @@ function start() {
                 type: 'input',
                 name: 'text',
                 message: 'What characters would you like to use for your text?',
+                //Check the input
                 validate: function (input) {
                     if (input.length <= 3) {
+                        //Accepts input if 3 characters are entered or less
                         return true;
                     } else {
                         return 'Please enter up to three characters';
@@ -46,6 +48,7 @@ function start() {
         //Choosing the shape
         .then((response) => {
             let userChoice;
+            //Create shape based on user input from choices
             switch (response.shape) {
                 case 'Circle':
                     userChoice = new Circle()
@@ -60,17 +63,22 @@ function start() {
 
             }
             //Creates a copy from the method of the shape and text color
+            //Generate shape color on user input
             userChoice.setColor(response.shapecolor)
             const myCoolLogo = new SVG()
+            //Generate shape on user input
             myCoolLogo.setShape(userChoice)
+            //Generate text color on user input
             myCoolLogo.setText(response.text, response.textColor)
 
             //Creates a new SVG file
             fs.writeFile('logo.svg', myCoolLogo.render(), (err) => {
+                //Provide error message if information fails
+                //Create the SVG file if information succeeds
                 err ? console.error(err) : console.log('Generated logo.svg')
             })
         });
 }
 
-
+//call the start function
 start();
